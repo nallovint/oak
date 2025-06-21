53ca76deab770cfb2f4829c763b8f679b268ce41
admodev <admodevcodes@outlook.com>
2025-06-21 19:41:52 -0300

    refactor: :recycle: changes tokenizer to use fsm
    Changes how the tokenizer tokenizes code. Now it uses a FSM (Finite State Machine) approach, this makes it possible to completelly detach from the use of regex to tokenize code

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -1,6 +1,4 @@
 // Tokenizer
-use regex::Regex;
-
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
     Var,
@@ -17,27 +15,68 @@ pub enum Token {
 
 pub fn tokenize(source: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
+    // Manual tokenization without regex (finite state machine) approach
+    let chars: Vec<char> = source.chars().collect();
+    let mut pos = 0;
 
-    let string_re = Regex::new(r#""([^"]*)""#).unwrap();
-    let number_re = Regex::new(r"^-?\d+(\.\d+)?$").unwrap();
+    while pos < chars.len() {
+        let c = chars[pos];
 
-    for word in source.split_whitespace() {
-        match word {
-            "var" => tokens.push(Token::Var),
-            ":=" => tokens.push(Token::Assign),
-            "+" | "-" | "*" | "/" => tokens.push(Token::Operator(word.to_string())),
-            _ => {
-                if word.starts_with("\"") && word.ends_with("\"") {
-                    if let Some(cap) = string_re.captures(word) {
-                        tokens.push(Token::StringLiteral(cap[1].to_string()));
-                    } else {
-                        tokens.push(Token::Unknown(word.to_string()));
-                    }
-                } else if number_re.is_match(word) {
-                    tokens.push(Token::Number(word.parse().unwrap()));
-                } else {
-                    tokens.push(Token::Identifier(word.to_string()));
+        match c {
+            // If the character si whitespace it continues until the
+            // tokenizer encounters a character to match
+            c if c.is_whitespace() => pos += 1,
+            // Assignation of values to variables and function output
+            ':' if pos + 1 < chars.len() && chars[pos + 1] == '=' => {
+                tokens.push(Token::Assign);
+                pos += 2;
+            }
+            '+' | '-' | '*' | '/' | '%' | '^' => {
+                tokens.push(Token::Operator(c.to_string()));
+                pos += 1;
+            }
+            '"' => {
+                pos += 1;
+                let start = pos;
+                while pos < chars.len() && chars[pos] != '"' {
+                    pos += 1;
                 }
+                let literal: String = chars[start..pos].iter().collect();
+                tokens.push(Token::StringLiteral(literal));
+                pos += 1; // consumes closing quote
+            }
+            // Analyses if the current token is an ascii_digit and parses it as a Number token
+            // In future releases, the language will implement different types of numerical values
+            // and different types of operations depending on the type of numerical value given to the interpreter/compiler
+            c if c.is_ascii_digit() => {
+                let start = pos;
+                while pos < chars.len() && (chars[pos].is_ascii_digit() || chars[pos] == '.') {
+                    pos += 1;
+                }
+                let number_str: String = chars[start..pos].iter().collect();
+                if let Ok(num) = number_str.parse::<f64>() {
+                    tokens.push(Token::Number(num));
+                } else {
+                    tokens.push(Token::Unknown(number_str));
+                }
+            }
+            // Gives names to variables (identifiers)
+            c if c.is_ascii_alphabetic() => {
+                let start = pos;
+                while pos < chars.len() && (chars[pos].is_ascii_alphanumeric() || chars[pos] == '_')
+                {
+                    pos += 1;
+                }
+                let ident: String = chars[start..pos].iter().collect();
+                match ident.as_str() {
+                    "var" => tokens.push(Token::Var),
+                    _ => tokens.push(Token::Identifier(ident)),
+                }
+            }
+            // ALL other type of tokens aren't parsed and identified as "unknown"
+            _ => {
+                tokens.push(Token::Unknown(c.to_string()));
+                pos += 1;
             }
         }
     }
